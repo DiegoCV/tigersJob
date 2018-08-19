18d65520e06d9833d973e94097855a4cf7d01ec6
DiegoCV <skdiegomxtr@gmail.com>
2018-08-19 11:08:07 -0500

    se puede iniciar la construccion

--- Editor de texto/form.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$var = $_GET['contenido'];
-echo $var;
---
+++ vista/gre.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+</head>
+<body>
+	
+        <form action="form.php">
+                        <input type="text" name="titulo"/>
+                        <input type="text" name="enlace"/>
+                        <textarea name="contenido">Next, use our Get Started docs to setup Tiny!</textarea>
+                        <button>Enviar</button>    
+                     </form>
+                     <script src="vista/tinymce/js/tinymce/tinymce.min.js"></script>
+      <script>tinymce.init({ selector:'textarea' });</script>
+</body>
+</html> 
